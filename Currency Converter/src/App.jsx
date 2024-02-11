4608a9aa8b5871384c755387c8e9836f3f85d771
Sanuka Sithum Gunarathna <106462234+sithumgithub@users.noreply.github.com>
2024-02-11 09:01:12 +0530

    first

---
+++ Currency Converter/src/App.jsx
@@ -0,0 +1,19 @@
+import { useState } from 'react'
+import './App.css'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <div className='w-full h-screen flex flex-wrap
+    justify-center items-center bg-cover bg-no-repeat'
+    style={{backgroundImage: `url(https://images.pexels.com/photos/3806749/pexels-photo-3806749.jpeg?auto=compress&cs=tinysrgb&w=400)`
+  }}
+    >
+
+    <h1>Test For tailwind</h1>
+    </div>
+  )
+}
+
+export default App
---
+++ Currency Converter/src/components/InputBox.jsx
@@ -0,0 +1,34 @@
+import React from 'react'
+
+function InputBox({
+    label,
+    amount,
+    onAmountChange,
+    onCurrencyChange,
+    currencyOptions = [],
+    selectedCurrency = "usd",
+    amountDiasabled = false,
+    currencyDisabled = false,
+    className = "",
+
+}) {
+  return (
+    <div className={`bg-white p-3 rounded-lg text-sm 
+    flex ${className}`}>
+      <div className="w-1-2">
+        <label className='text-black/40 mb-2 inline-block'>
+            {label}
+        </label>
+
+        <input
+        type='number'
+        className='outline-none w-full bg-transparent py-1.5' 
+        placeholder='Amount'
+        disabled={amountDiasabled}
+        value={amount}
+        />
+      </div>
+    </div>
+  )
+}
+ export default InputBox
